c952cd0e9377ef7bc1b9e6584de6d2c6e0a72a5b
Rvfg <i@rvf6.com>
2022-08-27 21:22:33 +0800

    rpi3: Add swap. Add ssh in initrd.

--- machines/rpi3/hardware-configuration.nix
+++ machines/rpi3/hardware-configuration.nix
@@ -1,12 +1,34 @@
 { lib, ... }:
 
 {
-  boot.initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd = {
+    availableKernelModules = [ "usbhid" "usb_storage" "lan78xx" ];
+    kernelModules = [ ];
+    luks.devices."cryptroot" = {
+      device = "/dev/disk/by-label/system_luks";
+      allowDiscards = true;
+      bypassWorkqueues = true;
+    };
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdmqOuypyBe2tF0fQ3R5vp9YkUg1e0lREno2ezJJE86"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL6r8qfrXMqjnUBhxuBSMt0cfjHo+Vhvqtod8vvwoQk4AAAABHNzaDo= canokey"
+        ];
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+      };
+    };
+  };
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-label/system_luks";
+  boot.kernelParams = [
+    "console=ttyS0,115200n8"
+    "console=ttyAMA0,115200n8"
+    "console=tty0"
+  ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/system";
@@ -20,12 +42,18 @@
       options = [ "subvol=NixOS/nix" "compress=zstd" "discard=async" "noatime" ];
     };
 
+  fileSystems."/swap" =
+    { device = "/dev/disk/by-label/system";
+      fsType = "btrfs";
+      options = [ "subvol=swap" "compress=zstd" "discard=async" "noatime" ];
+    };
+
   fileSystems."/boot" =
     { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/swap/swapfile"; } ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
