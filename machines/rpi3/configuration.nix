654968a8f3727dd781e96e00bffd79d05878acd0
Rvfg <i@rvf6.com>
2022-08-31 20:35:44 +0800

    rpi3: Add nftables

--- machines/rpi3/configuration.nix
+++ machines/rpi3/configuration.nix
@@ -10,6 +10,22 @@
   };
 
   networking.hostName = "rpi3";
+  networking.nftables = {
+    inputAccept = ''
+      udp dport 11112 accept comment "wireguard"
+    '';
+    masquerade = [ "oifname \"eth0\"" ];
+    tproxy = {
+      enable = false;
+      enableLocal = true;
+      src = ''
+        ip saddr 10.6.6.0/24 return;
+      '';
+      dst = ''
+        ip daddr 17.0.0.0/8 accept comment "Apple"
+      '';
+    };
+  };
 
   home-manager = {
     useGlobalPkgs = true;
