876bfcf41a654f5f1009d6043649f06a2e67ea13
Rvfg <i@rvf6.com>
2022-09-01 01:55:52 +0800

    rpi3: wireguard: Fix permission

--- machines/rpi3/configuration.nix
+++ machines/rpi3/configuration.nix
@@ -7,7 +7,7 @@
   sops = {
     defaultSopsFile = ./secrets.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    secrets.wireguard_key = {};
+    secrets.wireguard_key.owner = "systemd-network";
   };
 
   boot.loader = {
