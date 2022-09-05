b15c2f613417e29e798ea9a6356ffb1916858800
Rvfg <i@rvf6.com>
2022-09-06 00:40:57 +0800

    Add wg1

---
+++ lib/wg1.nix
@@ -0,0 +1,11 @@
+with builtins;
+rec {
+  port = 11111;
+  addrPre = "10.6.7.";
+  mask = 24;
+  gateway = "${addrPre}1";
+  gatewaySubnet = "${gateway}/${toString mask}";
+  subnet = "${addrPre}0/${toString mask}";
+  endpoint = "h.rvf6.com:${toString port}";
+  pubkey = "OXMopf5h0m7x2udIdCR7qxBhniN5+coCGqbrm99Lgi4=";
+}
