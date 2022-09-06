99e51897761f33d1ff27a1e3419daa1bd960a243
Rvfg <i@rvf6.com>
2022-09-06 16:43:39 +0800

    work: git: Sign using ssh key

--- machines/work/home.nix
+++ machines/work/home.nix
@@ -14,21 +14,21 @@
     wireguard-tools
   ];
 
+  home.file.".ssh/allowed_signers".text = "i@rvf6.com ${builtins.readFile ../../modules/ssh/id_canokey.pub}";
+
   programs.git = {
     enable = true;
     userEmail = "i@rvf6.com";
     userName = "Rvfg";
-    #signing = {
-    #  signByDefault = true;
-    #  key = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
-    #};
-    #extraConfig = {
-    #  init.defaultBranch = "main";
-    #  gcrypt = {
-    #    participants = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
-    #    publish-participants = true;
-    #  };
-    #};
+    signing = {
+      signByDefault = true;
+      key = "~/.ssh/id_canokey.pub";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+    };
   };
 
   programs.gpg.enable = true;
