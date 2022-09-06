50abbbfcca1c6e7c2716009ba197e38136867e5c
Rvfg <i@rvf6.com>
2022-09-06 15:48:31 +0800

    sshd: hostKeys: Use ed25519 only

--- modules/common.nix
+++ modules/common.nix
@@ -43,6 +43,7 @@
 
   services.openssh = {
     enable = true;
+    hostKeys = [ { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; } ];
     kbdInteractiveAuthentication = false;
     passwordAuthentication = false;
     permitRootLogin = "no";
