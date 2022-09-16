f1a1aa1f2e6d05fbb1cfee1c448a653e5a27c22b
Rvfg <i@rvf6.com>
2022-09-16 20:25:56 +0800

    rpi3: ns.nix: Add simns

--- machines/rpi3/ns.nix
+++ machines/rpi3/ns.nix
@@ -14,6 +14,28 @@ in {
     name = vethHost;
     address = [ "10.6.7.1/24" ];
   };
+  systemd.network.networks."50-simns" = {
+    name = "simns";
+    linkConfig = { MACAddress = "CC:5B:31:2F:BE:AD"; };
+    DHCP = "yes";
+    dhcpV4Config = {
+      SendHostname = false;
+      ClientIdentifier = "mac";
+      UseDNS = false;
+      UseNTP = false;
+      UseSIP = false;
+      UseDomains = false;
+      RouteTable = 10;
+    };
+    routingPolicyRules = [
+      {
+        routingPolicyRuleConfig = {
+          FirewallMark = 3;
+          Table = 10;
+        };
+      }
+    ];
+  };
 
   networking.nftables.forwardAccept = ''
     iifname veth-owrt oifname eth0 accept;
@@ -27,7 +49,7 @@ in {
     after =  [ "network.target" "modprobe@tun.service" "sys-devices-virtual-net-${vlan}.device" ];
     wantedBy = [ "machines.target" ];
     serviceConfig = {
-      ExecStart = "systemd-nspawn --quiet --keep-unit --boot --link-journal=no --network-interface=${vlan} --network-veth-extra=${vethHost}:${vethContainer} --private-users=pick --settings=no --machine=${machine}";
+      ExecStart = "systemd-nspawn --quiet --keep-unit --boot --link-journal=no --network-interface=${vlan} --network-veth-extra=${vethHost}:${vethContainer} --network-veth-extra=simns --private-users=pick --settings=no --machine=${machine} --kill-signal=SIGTERM";
       KillMode = "mixed";
       Type = "notify";
       RestartForceExitStatus = 133;
