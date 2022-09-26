c561f2311ef28f865ecab406cbf9300d00fa0877
Rvfg <i@rvf6.com>
2022-09-26 14:41:37 +0800

    Add home-modules: workstation.nix

---
+++ home-modules/workstation.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }: {
+  imports = [
+    ./common.nix
+    ./ssh.nix
+  ];
+
+  home.packages = with pkgs; [
+    ncdu
+    tdesktop
+  ];
+
+  programs.git = {
+    enable = true;
+    userEmail = "i@rvf6.com";
+    userName = "Rvfg";
+    signing = {
+      signByDefault = true;
+      key = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      gcrypt = {
+        participants = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
+        publish-participants = true;
+      };
+    };
+  };
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      forceWayland = true;
+    };
+  };
+
+  programs.mpv = {
+    enable = true;
+    config = {
+      fullscreen = true;
+    };
+  };
+}
--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -1,42 +1,5 @@
 { pkgs, ... }: {
   imports = [
-    ../../home-modules/common.nix
-    ../../home-modules/ssh.nix
+    ../../home-modules/workstation.nix
   ];
-
-  home.packages = with pkgs; [
-    ncdu
-    tdesktop
-  ];
-
-  programs.git = {
-    enable = true;
-    userEmail = "i@rvf6.com";
-    userName = "Rvfg";
-    signing = {
-      signByDefault = true;
-      key = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      gcrypt = {
-        participants = "F2E3DA8DE23F4EA11033EDEC535D184864C05736";
-        publish-participants = true;
-      };
-    };
-  };
-
-  programs.firefox = {
-    enable = true;
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-      forceWayland = true;
-    };
-  };
-
-  programs.mpv = {
-    enable = true;
-    config = {
-      fullscreen = true;
-    };
-  };
 }
