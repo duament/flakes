29a04b78ae6c40d05e436eb0fb6e840f7a814fb7
Rvfg <i@rvf6.com>
2022-09-26 14:57:35 +0800

    desktop: luks: Set bypassWorkqueues

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -2,10 +2,8 @@
 {
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.luks.devices.cryptroot.allowDiscards = true;
 
   networking.hostName = "desktop";
-  # networking.wireless.enable = true;
 
   home-manager.users.rvfg = import ./home.nix;
 
--- machines/desktop/hardware-configuration.nix
+++ machines/desktop/hardware-configuration.nix
@@ -1,12 +1,17 @@
 { lib, ... }:
 
 {
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd = {
+    availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    kernelModules = [ ];
+    luks.devices."cryptroot" = {
+      device = "/dev/disk/by-uuid/1c243edd-9a38-485e-8be4-b864d9736e1e";
+      allowDiscards = true;
+      bypassWorkqueues = true;
+    };
+  };
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/1c243edd-9a38-485e-8be4-b864d9736e1e";
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/75dffd2b-9f67-4428-9fc4-67298fea3528";
