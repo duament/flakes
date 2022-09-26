7ef0d2d8c3a6ac8c420a57957abf3d2489dbd828
Rvfg <i@rvf6.com>
2022-09-26 12:26:49 +0800

    desktop: Use ssh module

--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -1,62 +1,13 @@
 { pkgs, ... }: {
   imports = [
     ../../modules/common-home.nix
+    ../../modules/ssh
   ];
 
   home.packages = with pkgs; [
     ncdu
     tdesktop
   ];
-
-  programs.ssh = let
-    sshIdentities = [ "~/.ssh/id_ed25519.pub" "~/.ssh/id_canokey" "~/.ssh/id_a4b" ];
-  in {
-    enable = true;
-    compression = true;
-    serverAliveInterval = 10;
-    matchBlocks = {
-      "nl" = {
-        user = "duama";
-        hostname = "nl.rvf6.com";
-        identityFile = sshIdentities;
-        forwardAgent = true;
-      };
-      "az" = {
-        user = "duama";
-        hostname = "az.rvf6.com";
-        identityFile = sshIdentities;
-        forwardAgent = true;
-      };
-      "or1" = {
-        user = "duama";
-        hostname = "or1.rvf6.com";
-        identityFile = sshIdentities;
-        forwardAgent = true;
-      };
-      "or2" = {
-        user = "duama";
-        hostname = "or2.rvf6.com";
-        identityFile = sshIdentities;
-        forwardAgent = true;
-      };
-      "or3" = {
-        user = "duama";
-        hostname = "or3.rvf6.com";
-        identityFile = sshIdentities;
-        forwardAgent = true;
-      };
-      "aur.archlinux.org" = {
-        user = "aur";
-        identityFile = [ "~/.ssh/id_aur.pub" "~/.ssh/id_aur_canokey" ];
-      };
-      "github.com" = {
-        identityFile = [ "~/.ssh/id_ed25519.pub" "~/.ssh/id_github_canokey" ];
-      };
-    };
-    extraConfig = ''
-      CheckHostIP no
-    '';
-  };
 
   programs.git = {
     enable = true;
