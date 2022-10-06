6db097aab78e0d5547dcbbec7689c47ac06fbc2d
Rvfg <i@rvf6.com>
2022-10-06 23:34:42 +0800

    wg0: Add k1 and k2

--- lib/wg0.nix
+++ lib/wg0.nix
@@ -14,13 +14,21 @@ rec {
       ip = "${addrPre}2";
       pubkey = "BcLh8OUygmCL2m50MREgsAwOLMkF9A+eAhuQDEPaqWI=";
     };
-    work = {
-      ip = "${addrPre}10";
-      pubkey = "Zwg16+rw0uVJHFWsKj56nc9+eli0/XIYrKxespMbGj0=";
-    };
     xiaoxin = {
       ip = "${addrPre}3";
       pubkey = "EB86zlOPDzQLKkByxWznf/deQiIssQ6LAcMuw2oDbRI=";
+    };
+    k2 = {
+      ip = "${addrPre}4";
+      pubkey = "Q+jZnY03JJc20cjwsP60CeClbxhYV9OU0jAEPqKNjTY=";
+    };
+    k1 = {
+      ip = "${addrPre}5";
+      pubkey = "XQss9kzHipmjizrqUsBYQltHuj3NvyR8+e0hLRfunQE=";
+    };
+    work = {
+      ip = "${addrPre}10";
+      pubkey = "Zwg16+rw0uVJHFWsKj56nc9+eli0/XIYrKxespMbGj0=";
     };
   };
   peerConfigs = map (peer: {
