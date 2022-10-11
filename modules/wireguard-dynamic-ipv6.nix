0e8e653986fe23ebc5716e798cf08bc54ae7f3c0
Rvfg <i@rvf6.com>
2022-10-11 19:34:50 +0800

    modules/wireguard-dynamic-ipv6.nix: Fix script

--- modules/wireguard-dynamic-ipv6.nix
+++ modules/wireguard-dynamic-ipv6.nix
@@ -18,7 +18,7 @@ in {
         path = [ pkgs.iproute2 pkgs.jq pkgs.sipcalc pkgs.gawk pkgs.wireguard-tools ];
         script = ''
           set -o pipefail
-          IPV6=$(ip -j -6 a show dev ${interface} scope global | jq '.[0].addr_info | .[0].local')
+          IPV6=$(ip -j -6 a show dev ${interface} scope global | jq -r '.[0].addr_info | .[0].local')
           IPV6_EXPANDED=$(sipcalc -6 "$IPV6" | grep 'Expanded Address' | awk '{print $NF}')
           IPV6_PREFIX=''${IPV6_EXPANDED%:*:*:*:*}
           if [[ -z "$IPV6_PREFIX" ]]; then exit; fi
@@ -26,12 +26,14 @@ in {
             pubkey=$(echo "$line" | awk '{print $1}')
             ipv4=$(echo "$line" | awk '{print $2}')
             ipv6=$(echo "$line" | awk '{print $3}')
-            ipv4_host=$(sipcalc -4 "$ipv4" | grep 'Host address' | awk '{print $NF}')
+            ipv4_host=$(sipcalc -4 "$ipv4" | grep 'Host address' | head -n 1 | awk '{print $NF}')
             host_num=''${ipv4_host##*.}
             if [[ -z "$host_num" ]]; then exit; fi
             host_hex=$(printf '%x' "$host_num")
-            ipv6_expanded=$(sipcalc -6 "$ipv6" | grep 'Expanded Address' | awk '{print $NF}')
-            ipv6_prefix=''${ipv6_expanded%:*:*:*:*}
+            if [[ -n "$ipv6" ]]; then
+              ipv6_expanded=$(sipcalc -6 "$ipv6" | grep 'Expanded Address' | awk '{print $NF}')
+              ipv6_prefix=''${ipv6_expanded%:*:*:*:*}
+            fi
             if [[ "IPV6_PREFIX" != "$ipv6_prefix" ]]; then
               wg set ${interface} peer "$pubkey" allowed-ips "$ipv4,$IPV6_PREFIX::$host_hex/128"
             fi
