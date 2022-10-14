4e24f039f8a76c7c724f5d503eca65da11f20166
Rvfg <i@rvf6.com>
2022-10-14 11:16:52 +0800

    rpi3: Exclude or3 in smartdns

--- machines/rpi3/configuration.nix
+++ machines/rpi3/configuration.nix
@@ -71,7 +71,7 @@ in {
   ];
   services.smartdns.chinaDns = [ "192.168.2.1" ];
   services.smartdns.settings.address = with builtins;
-    attrValues (mapAttrs (name: value: "/${name}.rvf6.com/${value.ip}") wg0.peers) ++ [
+    filter (i: i != "") (attrValues (mapAttrs (name: value: if (value ? endpointAddr) then "" else "/${name}.rvf6.com/${value.ip}") wg0.peers)) ++ [
       "/rpi3.rvf6.com/${wg0.gateway}"
       "/owrt.rvf6.com/192.168.2.1"
       "/t430.rvf6.com/192.168.2.8"
