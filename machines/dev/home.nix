4a9ad9b64fe4724680789e0466226cba09ff697f
Rvfg <i@rvf6.com>
2022-10-25 09:57:02 +0800

    machines/dev/home.nix: Add https://cache.nixos.org/

--- machines/dev/home.nix
+++ machines/dev/home.nix
@@ -11,6 +11,7 @@
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
     ];
   };
 
