b084ad4a061412d0b03cc68bcd213758710109cd
Rvfg <i@rvf6.com>
2022-10-30 13:47:00 +0800

    machines/t430: Add IKEv2 VPN

--- machines/t430/configuration.nix
+++ machines/t430/configuration.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ config, lib, ... }:
 {
   presets.nogui.enable = true;
 
   sops.defaultSopsFile = ./secrets.yaml;
-  sops.secrets.initrd_ssh_host_ed25519_key = {};
+  sops.secrets = {
+    initrd_ssh_host_ed25519_key = {};
+    swanctl = {};
+  };
 
   boot.loader.generationsDir.copyKernels = true;
   boot.loader.systemd-boot.enable = true;
@@ -19,4 +22,28 @@
   home-manager.users.rvfg = import ./home.nix;
 
   services.uu.enable = true;
+
+  services.strongswan-swanctl.enable = true;
+  services.strongswan-swanctl.strongswan.extraConfig = ''
+    charon {
+      plugins {
+        attr {
+          dns = 223.5.5.5
+        }
+      }
+    }
+  '';
+  environment.etc."swanctl/swanctl.conf".enable = false;
+  system.activationScripts.strongswan-swanctl-secret-conf = lib.stringAfter ["etc"] ''
+    mkdir -p /etc/swanctl
+    ln -sf ${config.sops.secrets.swanctl.path} /etc/swanctl/swanctl.conf
+  '';
+  networking.nftables.inputAccept = ''
+    ip protocol { ah, esp } accept
+    udp dport { 500, 4500 } accept
+  '';
+  networking.nftables.forwardAccept = ''
+    meta ipsec exists accept
+    rt ipsec exists accept
+  '';
 }
