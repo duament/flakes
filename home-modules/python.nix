352f3730baf3121c8afecf3b8e4ffa0d32643ce9
Rvfg <i@rvf6.com>
2022-11-08 10:43:26 +0800

    home-modules/python.nix: Add extraPackages option

--- home-modules/python.nix
+++ home-modules/python.nix
@@ -1,17 +1,23 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
+  cfg = config.presets.python;
   my-python-packages = python-packages: with python-packages; [
     python-lsp-server
     ipython
     requests
-  ];
+  ] ++ cfg.extraPackages python-packages;
   python-with-my-packages = pkgs.python3.withPackages my-python-packages;
 in {
   options = {
     presets.python.enable = mkOption {
       type = types.bool;
       default = false;
+    };
+
+    presets.python.extraPackages = mkOption {
+      type = types.functionTo (types.listOf types.package);
+      default = _: [];
     };
   };
 
