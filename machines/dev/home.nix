6b2cb9a2be96bc73bf0f002ac4dbaa44c8ca8371
Rvfg <i@rvf6.com>
2022-12-26 13:59:21 +0800

    machines/dev/home.nix: Add SJTU mirror

--- machines/dev/home.nix
+++ machines/dev/home.nix
@@ -11,6 +11,7 @@
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
   };
