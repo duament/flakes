02ac1fb0438c3bbf594575b597f4b1bf9fe4784f
Rvfg <i@rvf6.com>
2023-03-07 17:06:51 +0800

    Add modules/bpf-mark.nix

---
+++ modules/bpf-mark.nix
@@ -0,0 +1,39 @@
+{ config, lib, mypkgs, pkgs, ... }:
+let
+  cfg = config.presets.bpf-mark;
+
+  markValues = lib.unique (builtins.attrValues cfg);
+in
+{
+  options = {
+    presets.bpf-mark = lib.mkOption {
+      type = with lib.types; attrsOf int;
+      default = { };
+      example = { "serive1" = 1; };
+    };
+  };
+
+  config = {
+    systemd.services = builtins.listToAttrs
+      (map
+        (markValue: {
+          name = "bpf-mark-${toString markValue}";
+          value = {
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart = "${pkgs.bpftool}/bin/bpftool prog load ${mypkgs.bpf-mark.override { inherit markValue; }} /sys/fs/bpf/mark-${toString markValue} type cgroup/sock";
+              ExecStop = "${pkgs.coreutils}/bin/rm -f /sys/fs/bpf/mark-${toString markValue}";
+              RemainAfterExit = true;
+            };
+          };
+        })
+        markValues) // builtins.mapAttrs
+      (name: value: {
+        requires = [ "bpf-mark-${toString value}.service" ];
+        after = [ "bpf-mark-${toString value}.service" ];
+        serviceConfig.BPFProgram = "sock_create:/sys/fs/bpf/mark-${toString value}";
+      })
+      cfg;
+  };
+}
--- modules/default.nix
+++ modules/default.nix
@@ -3,6 +3,7 @@
   imports = [
     inputs.home-manager.nixosModules.home-manager
     inputs.sops-nix.nixosModules.sops
+    ./bpf-mark.nix
     ./chromium.nix
     ./clash.nix
     ./common.nix
