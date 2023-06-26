9f593a3309abf5de8cc7d154252e0519799c1fb4
Rvfg <i@rvf6.com>
2023-06-26 21:06:57 +0800

    home-modules/hyprland: Add external monitor

--- machines/xiaoxin/home.nix
+++ machines/xiaoxin/home.nix
@@ -5,7 +5,8 @@
 
   presets.workstation.enable = true;
   xdg.configFile."hypr/hyprland.conf".text = ''
-    monitor = eDP-1, preferred, auto, 2
+    monitor = eDP-1, preferred, 320x1080, 2
+    monitor = , preferred, 0x0, 2
   '';
 
   home.packages = with pkgs; [
