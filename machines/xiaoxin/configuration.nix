54c6742edb75fe3912c9cd8bef398d1b0895cd8a
Rvfg <i@rvf6.com>
2023-07-16 21:00:38 +0800

    machines/xiaoxin: Switch to StrongSwan

--- machines/xiaoxin/configuration.nix
+++ machines/xiaoxin/configuration.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   nixpkgs.overlays = [
     (self: super: {
@@ -22,9 +22,11 @@
       sopsFile = ../../secrets/clash;
     };
     wireguard_key.owner = "systemd-network";
-    "eap/ca" = { };
-    "eap/xiaoxin-bundle" = { };
-    "eap/xiaoxin-key" = { };
+    "pki/ca" = { };
+    "pki/ybk" = { };
+    "pki/xiaoxin-bundle" = { };
+    "pki/xiaoxin-key" = { };
+    "pki/xiaoxin-pkcs8-key" = { };
     "syncthing/cert".owner = config.services.syncthing.user;
     "syncthing/key".owner = config.services.syncthing.user;
   };
@@ -49,14 +51,59 @@
           pairwise=GCMP-256
           group=GCMP-256
           identity="xiaoxin@rvf6.com"
-          ca_cert="${config.sops.secrets."eap/ca".path}"
-          client_cert="${config.sops.secrets."eap/xiaoxin-bundle".path}"
-          private_key="${config.sops.secrets."eap/xiaoxin-key".path}"
+          ca_cert="${config.sops.secrets."pki/ca".path}"
+          client_cert="${config.sops.secrets."pki/xiaoxin-bundle".path}"
+          private_key="${config.sops.secrets."pki/xiaoxin-key".path}"
         '';
       };
     };
   };
-  systemd.network.networks."99-wireless-client-dhcp".domains = [ "~h.rvf6.com" ];
+  systemd.network.networks."99-wireless-client-dhcp".linkConfig.RequiredForOnline = true;
+
+  presets.wireguard.wg0.enable = lib.mkForce false;
+  system.activationScripts.strongswan-swanctl-private = lib.stringAfter [ "etc" ] ''
+    mkdir -p /etc/swanctl/private
+    ln -sf ${config.sops.secrets."pki/${config.networking.hostName}-pkcs8-key".path} /etc/swanctl/private/${config.networking.hostName}.key
+  '';
+  services.strongswan-swanctl = {
+    enable = true;
+    swanctl.connections.t430 = {
+      local.${config.networking.hostName} = {
+        auth = "pubkey";
+        id = "${config.networking.hostName}@rvf6.com";
+        certs = [ config.sops.secrets."pki/${config.networking.hostName}-bundle".path ];
+      };
+      remote.t430 = {
+        auth = "pubkey";
+        id = "t430.rvf6.com";
+        cacerts = [ config.sops.secrets."pki/ca".path config.sops.secrets."pki/ybk".path ];
+      };
+      children.t430 = {
+        local_ts = [ "0.0.0.0/0" "::/0" ];
+        remote_ts = [ "0.0.0.0/0" "::/0" ];
+        # start_action = "trap";
+        start_action = "start";
+        dpd_action = "restart";
+      };
+      remote_addrs = [ "h.rvf6.com" ];
+      version = 2;
+      vips = [ "0.0.0.0" "::" "fd64::1" ];
+      proposals = [ "aes256gcm16-prfsha384-curve25519" "aes256gcm16-prfsha384-ecp384" ];
+    };
+    strongswan.extraConfig = ''
+      charon {
+        plugins {
+          resolve {
+            resolvconf {
+              iface = wlp1s0
+              path = ${config.networking.resolvconf.package}/bin/resolvconf
+            }
+          }
+        }
+      }
+    '';
+  };
+  systemd.services.strongswan-swanctl.after = [ "systemd-networkd-wait-online.service" ];
 
   home-manager.users.rvfg = import ./home.nix;
 
