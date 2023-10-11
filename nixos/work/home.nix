33b562d0785fed0ac3e938471710a1a2e7fb5f62
Rvfg <i@rvf6.com>
2023-10-11 11:20:39 +0800

    nixos/work/home.nix: Add ssh config

--- nixos/work/home.nix
+++ nixos/work/home.nix
@@ -5,6 +5,39 @@
   ];
 
   presets.ssh.enable = true;
+  programs.ssh.matchBlocks = rec {
+    devbash = {
+      user = "ruifeng.ma";
+      hostname = "10.9.112.14";
+    };
+    dev = devbash // {
+      forwardAgent = true;
+      extraOptions = {
+        RequestTTY = "force";
+        RemoteCommand = ". ~/n.sh";
+      };
+    };
+    devroot = devbash // {
+      extraOptions = {
+        RequestTTY = "force";
+        RemoteCommand = "~/root";
+      };
+    };
+    sit = {
+      user = "root";
+      hostname = "10.9.114.25";
+    };
+    jump = {
+      user = "root";
+      hostname = "10.9.231.27";
+    };
+    prod = {
+      user = "root";
+      hostname = "172.21.3.17";
+      proxyJump = "jump";
+    };
+  };
+
   presets.git.enable = true;
   presets.python.enable = true;
 
