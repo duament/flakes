b6b2840a34b8a4a7853f25039662126114b1dc17
Rvfg <i@rvf6.com>
2023-12-17 21:51:27 +0800

    modules/avbroot.nix: Select latest patch level

--- modules/avbroot.nix
+++ modules/avbroot.nix
@@ -5,6 +5,31 @@ let
   avbroot-bin = "${pkgs.avbroot}/bin/avbroot";
 
   kernelsu = ''curl -Ls -H "Authorization: Bearer $(cat ${config.sops.secrets.github-token.path})" https://api.github.com/repos/tiann/KernelSU/releases/latest'';
+
+  parse-ksu = pkgs.writers.writePython3 "parse-ksu" { } ''
+    import json
+    import re
+    import sys
+
+    prefix = sys.argv[1]
+
+    result = None
+    max_patch_level = -1
+
+    data = json.load(sys.stdin)
+    for asset in data.get('assets', []):
+        name = asset.get('name', ''')
+        if name.startswith(prefix) and name.endswith('boot-lz4.img.gz'):
+            patch_level = int(re.match(r'^[^-]*-\d+.\d+.(\d+)', name).group(1))
+            if patch_level > max_patch_level:
+                max_patch_level = patch_level
+                result = asset.get('browser_download_url')
+
+    if not result:
+        raise ValueError(f'result is {result}')
+    print(result, end=''')
+  '';
+
   avbroot-patch = pkgs.writeShellApplication {
     name = "avbroot-patch";
     text = ''
@@ -16,8 +41,9 @@ let
       VERSION=$(strings "$DIR"/boot/kernel | grep -o -m1 'Linux version [^-]*-[^-]*' || true)  # Linux version 5.10.157-android13
       ANDROID_RELEASE=''${VERSION##*-}  # android13
       VERSION=''${VERSION%-*}  # Linux version 5.10.157
-      KERNEL_VERSION=''${VERSION##* }  # 5.10.157
-      URL=$(${kernelsu} | ${pkgs.jq}/bin/jq -r ".assets[] | select(.name | startswith(\"$ANDROID_RELEASE-$KERNEL_VERSION\") and endswith(\"boot-lz4.img.gz\")) | .browser_download_url")
+      VERSION=''${VERSION##* }  # 5.10.157
+      KERNEL_VERSION=''${VERSION%.*}  # 5.10
+      URL=$(${kernelsu} | ${parse-ksu} "$ANDROID_RELEASE-$KERNEL_VERSION")
       NAME=''${URL##*/}
 
       ${pkgs.curl}/bin/curl -Ls -o "$DIR/$NAME" "$URL"
