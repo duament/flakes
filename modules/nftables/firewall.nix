329964deb70f4ea065ad507f8fd30d175560b1dd
Rvfg <i@rvf6.com>
2024-01-17 13:20:04 +0800

    modules/nftables/firewall.nix: apply MSS clamping in postrouting

--- modules/nftables/firewall.nix
+++ modules/nftables/firewall.nix
@@ -29,8 +29,8 @@ in
         ''}
         ${optionalString cfg.mssClamping ''
           chain mss-clamping {
-            type filter hook forward priority mangle;
-            tcp flags syn tcp option maxseg size set rt mtu
+            type filter hook postrouting priority mangle;
+            tcp flags syn / syn,fin,rst tcp option maxseg size set rt mtu
           }
         ''}
       '';
