859b6a76917f310cd715795aa00e3175f3d59bc3
Rvfg <i@rvf6.com>
2024-02-02 21:15:52 +0800

    nixos/rpi3: enable warp

--- nixos/rpi3/configuration.nix
+++ nixos/rpi3/configuration.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ config, ... }:
+let
+  nonCNMark = 2;
+in
 {
   presets.nogui.enable = true;
 
   sops.defaultSopsFile = ./secrets.yaml;
   sops.secrets = {
     initrd_ssh_host_ed25519_key = { };
+    warp_key.owner = "systemd-network";
   };
 
   boot.loader.generationsDir.copyKernels = true;
@@ -24,6 +28,68 @@
       enable = true;
       parentName = "10-enu1u1u1";
     };
+  };
+
+  networking.warp = {
+    enable = true;
+    endpointAddr = "162.159.192.1";
+    mtu = 1412;
+    mark = 3;
+    routingId = "0x29478c";
+    keyFile = config.sops.secrets.warp_key.path;
+    address = [ "172.16.0.2/32" "2606:4700:110:81f3:a59c:d5a7:d339:59b8/128" ];
+    table = 20;
+  };
+  presets.wireguard.keepAlive.interfaces = [ "warp" ];
+  networking.nftables.mssClamping = true;
+  networking.nftables.markChinaIP = {
+    enable = true;
+    mark = nonCNMark;
+  };
+  systemd.network.networks."25-warp".routingPolicyRules =
+    let
+      table = 20;
+    in
+    [
+      {
+        routingPolicyRuleConfig = {
+          FirewallMark = nonCNMark;
+          Table = table;
+          Priority = 20;
+          Family = "both";
+        };
+      }
+      {
+        routingPolicyRuleConfig = {
+          To = "34.117.196.143"; # prod-ingress.nianticlabs.com
+          Table = "main";
+          Priority = 9;
+        };
+      }
+      {
+        routingPolicyRuleConfig = {
+          To = "2001:da8:215:4078:250:56ff:fe97:654d"; # byr.pt
+          Table = table;
+          Priority = 9;
+        };
+      }
+    ];
+  presets.smartdns = {
+    enable = true;
+    chinaDns = [ "[fd69:2011:6f1d::1]" ];
+    settings.address = [
+      "/t430.rvf6.com/10.6.6.1"
+      "/t430.rvf6.com/fd64::1"
+      "/ax6s.rvf6.com/fd65::1"
+      "/ax6s.rvf6.com/-4"
+      "/rpi3.rvf6.com/10.7.0.7"
+      "/fava.rvf6.com/fd64::1"
+      "/fava.rvf6.com/-4"
+      "/luci.rvf6.com/fd64::1"
+      "/luci.rvf6.com/-4"
+      "/ha.rvf6.com/fd64::1"
+      "/ha.rvf6.com/-4"
+    ];
   };
 
   home-manager.users.rvfg = import ./home.nix;
