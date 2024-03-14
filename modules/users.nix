ee249e498b617d2b3659d25864bd9eafe9d090ca
Rvfg <i@rvf6.com>
2024-03-14 23:59:20 +0800

    modules/users.nix: use regex to match switch-to-configuration in sudoers

--- modules/users.nix
+++ modules/users.nix
@@ -51,23 +51,11 @@ in
             options = [ "NOPASSWD" ];
           }
           {
-            command = "/nix/store/*/bin/switch-to-configuration";
+            command = "^/nix/store/[^/ ]*/bin/switch-to-configuration$";
             options = [ "NOPASSWD" ];
           }
           {
-            command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pty --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait true";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pty --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait /nix/store/*/bin/switch-to-configuration *";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pipe --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait true";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pipe --quiet --same-dir --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait /nix/store/*/bin/switch-to-configuration *";
+            command = "/run/current-system/sw/bin/systemd-run ^((-E |--)[^ ]* )*(true|/nix/store/[^/ ]*/bin/switch-to-configuration (switch|boot))$";
             options = [ "NOPASSWD" ];
           }
         ];
