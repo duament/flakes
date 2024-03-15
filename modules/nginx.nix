9f068aff514606408a345bb31a37e80aa5db3cd7
Rvfg <i@rvf6.com>
2024-03-15 13:27:04 +0800

    modules/nginx.nix: advertise HTTP/3

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -44,7 +44,10 @@ in
             forceSSL = true;
             enableACME = cfg.useACMEHost == null;
             inherit (cfg) useACMEHost;
-            extraConfig = "add_header Strict-Transport-Security \"max-age=63072000; includeSubDomains; preload\" always;";
+            extraConfig = ''
+              add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+              add_header Alt-Svc 'h3=":$server_port"; ma=86400';
+            '';
             quic = true;
             http3 = true;
           }
