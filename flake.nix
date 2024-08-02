ac2745520219e7a0ef867302714d4d9257bf1101
Rvfg <i@rvf6.com>
2024-08-03 00:24:36 +0800

    Bump lanzaboote

--- flake.nix
+++ flake.nix
@@ -18,9 +18,8 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     chn-cidr-list = {
