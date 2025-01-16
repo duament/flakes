050d9758a326ff184c40150b0de9482fd494bef8
Rvfg <i@rvf6.com>
2025-01-16 15:23:44 +0800

    modules/workstation.nix: update pulseaudio module name

--- modules/workstation.nix
+++ modules/workstation.nix
@@ -194,7 +194,7 @@ in
     security.polkit.enable = true;
 
     security.rtkit.enable = true;
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     services.pipewire = {
       enable = true;
       #alsa.enable = true;
