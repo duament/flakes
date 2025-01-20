b0976a0b0a495af753e6b76077cefee144c61622
Rvfg <i@rvf6.com>
2025-01-21 01:50:10 +0800

    home-modules/workstation.nix: workaround home-manager #6329

--- home-modules/workstation.nix
+++ home-modules/workstation.nix
@@ -153,6 +153,10 @@
       enable = true;
       indicator = true;
     };
+    systemd.user.targets.tray.Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+    };
 
     programs.alacritty = {
       enable = true;
