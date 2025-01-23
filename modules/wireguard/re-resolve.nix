6df68a4ffaa5ace20509868213a04681464e800b
Rvfg <i@rvf6.com>
2025-01-23 18:05:20 +0800

    modules/wireguard/re-resolve.nix: refactor

--- modules/wireguard/re-resolve.nix
+++ modules/wireguard/re-resolve.nix
@@ -8,59 +8,68 @@
 with lib;
 let
   cfg = config.presets.wireguard.reResolve;
+
+  interfaces = attrNames cfg;
+
+  interfaceOptions =
+    { ... }:
+    {
+      options = {
+        address = mkOption {
+          type = types.str;
+        };
+
+        pubkey = mkOption {
+          type = types.str;
+        };
+      };
+    };
 in
 {
   options = {
-    presets.wireguard.reResolve.interfaces = mkOption {
-      type = types.listOf types.str;
-      default = [ ];
+    presets.wireguard.reResolve = mkOption {
+      type = types.attrsOf (types.submodule interfaceOptions);
+      default = { };
     };
   };
 
   config = {
-    systemd.services = builtins.listToAttrs (
-      map (
-        interface:
-        let
-          wg = self.data.${interface};
-          control = wg.peers.${wg.control};
-        in
-        {
-          name = "${interface}-re-resolve";
-          value = {
-            wants = [ "network-online.target" ];
-            after = [ "network-online.target" ];
-            path = [
-              pkgs.wireguard-tools
-              pkgs.gawk
-            ];
-            script = ''
-              set -o pipefail
-              ENDPOINT=$(wg show ${interface} endpoints | grep ${control.pubkey} | awk '{print $2}')
-              IP_IN_USE=''${ENDPOINT%%:*}
-              PORT=''${ENDPOINT##*:}
-              IP_RESOLVED=$(resolvectl query -4 --legend=no ${control.addr} | awk '{print $2}')
-              if [[ "$IP_IN_USE" != "$IP_RESOLVED" ]]; then
-                wg set ${interface} peer ${control.pubkey} endpoint "$IP_RESOLVED:$PORT"
-              fi
-            '';
-            serviceConfig = self.data.systemdHarden // {
-              Type = "oneshot";
-              AmbientCapabilities = [ "CAP_NET_ADMIN" ];
-              CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
-              RestrictAddressFamilies = [
-                "AF_UNIX"
-                "AF_INET"
-                "AF_INET6"
-                "AF_NETLINK"
-              ];
-              PrivateNetwork = false;
-              PrivateUsers = false;
-            };
-          };
-        }
-      ) cfg.interfaces
-    );
+    systemd.services = mapAttrs' (interface: option: {
+      name = "${interface}-re-resolve";
+      value = {
+        wants = [ "network-online.target" ];
+        after = [ "network-online.target" ];
+        path = [
+          pkgs.wireguard-tools
+          pkgs.gawk
+          pkgs.curl
+          pkgs.jq
+        ];
+        script = ''
+          set -o pipefail
+          ENDPOINT=$(wg show ${interface} endpoints | grep ${option.pubkey} | awk '{print $2}')
+          IP_IN_USE=''${ENDPOINT%%:*}
+          PORT=''${ENDPOINT##*:}
+          IP_RESOLVED=$(curl -s --retry 3 -m 60 --fail "https://223.5.5.5/resolve?name=t430-rvfg.duckdns.org&type=1" | jq -r '.Answer[0].data')
+          if [[ "$IP_IN_USE" != "$IP_RESOLVED" ]]; then
+            wg set ${interface} peer ${option.pubkey} endpoint "$IP_RESOLVED:$PORT"
+          fi
+        '';
+        serviceConfig = self.data.systemdHarden // {
+          Type = "oneshot";
+          AmbientCapabilities = [ "CAP_NET_ADMIN" ];
+          CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
+          RestrictAddressFamilies = [
+            "AF_UNIX"
+            "AF_INET"
+            "AF_INET6"
+            "AF_NETLINK"
+          ];
+          PrivateNetwork = false;
+          PrivateUsers = false;
+        };
+      };
+    }) cfg;
 
     systemd.timers = builtins.listToAttrs (
       map (interface: {
@@ -72,7 +81,7 @@ in
             OnUnitActiveSec = 600;
           };
         };
-      }) cfg.interfaces
+      }) interfaces
     );
   };
 }
