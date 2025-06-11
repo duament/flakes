a8ed996c3f5a4bd7b94e3027a19af66fcc634794
Rvfg <i@rvf6.com>
2025-06-11 22:42:38 +0800

    home-modules/browser.nix: migrate firefox bookmark settings

--- home-modules/browser.nix
+++ home-modules/browser.nix
@@ -65,79 +65,82 @@ with lib;
         };
       };
       profiles.default = {
-        bookmarks = [
-          {
-            name = "Miniflux";
-            url = "https://rss.rvf6.com";
-          }
-          {
-            name = "GitHub";
-            url = "https://github.com";
-          }
-          {
-            name = "ACG sub";
-            url = "https://bbs.acgrip.com/forum-37-1.html";
-          }
-          {
-            name = "Nyaa";
-            url = "https://nyaa.si/";
-          }
-          {
-            name = "Search";
-            bookmarks = [
-              {
-                name = "zdic";
-                keyword = "zd";
-                url = "https://www.zdic.net/search/?sclb=tm&q=%s";
-              }
-              {
-                name = "知乎";
-                keyword = "zhi";
-                url = "https://www.google.com/search?hl=en-US&gl=US&newwindow=0&q=site%3Awww.zhihu.com+%s";
-              }
-              {
-                name = "moegirl";
-                keyword = "moe";
-                url = "https://zh.moegirl.org.cn/index.php?search=%s";
-              }
-              {
-                name = "V2EX";
-                keyword = "v2";
-                url = "https://www.google.com/search?hl=en-US&gl=US&newwindow=0&q=site%3Av2ex.com+%s";
-              }
-              {
-                name = "维基百科";
-                keyword = "zh";
-                url = "https://zh.wikipedia.org/w/index.php?title=Special:%E6%90%9C%E7%B4%A2&search=%s";
-              }
-              {
-                name = "Baidu";
-                keyword = "bd";
-                url = "https://www.baidu.com/s?wd=%s";
-              }
-              {
-                name = "Arch manual pages";
-                keyword = "man";
-                url = "https://man.archlinux.org/search?q=%s";
-              }
-              {
-                name = "NixOS Search";
-                keyword = "nixp";
-                url = "https://search.nixos.org/packages?channel=unstable&type=packages&query=%s";
-              }
-              {
-                name = "ArchWiki";
-                keyword = "aw";
-                url = "https://wiki.archlinux.org/index.php?title=Special%3ASearch&wprov=acrw1&search=%s";
-              }
-              {
-                name = "Wikipedia";
-                keyword = "wk";
-                url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-              }
-            ];
-          }
-        ];
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "Miniflux";
+              url = "https://rss.rvf6.com";
+            }
+            {
+              name = "GitHub";
+              url = "https://github.com";
+            }
+            {
+              name = "ACG sub";
+              url = "https://bbs.acgrip.com/forum-37-1.html";
+            }
+            {
+              name = "Nyaa";
+              url = "https://nyaa.si/";
+            }
+            {
+              name = "Search";
+              bookmarks = [
+                {
+                  name = "zdic";
+                  keyword = "zd";
+                  url = "https://www.zdic.net/search/?sclb=tm&q=%s";
+                }
+                {
+                  name = "知乎";
+                  keyword = "zhi";
+                  url = "https://www.google.com/search?hl=en-US&gl=US&newwindow=0&q=site%3Awww.zhihu.com+%s";
+                }
+                {
+                  name = "moegirl";
+                  keyword = "moe";
+                  url = "https://zh.moegirl.org.cn/index.php?search=%s";
+                }
+                {
+                  name = "V2EX";
+                  keyword = "v2";
+                  url = "https://www.google.com/search?hl=en-US&gl=US&newwindow=0&q=site%3Av2ex.com+%s";
+                }
+                {
+                  name = "维基百科";
+                  keyword = "zh";
+                  url = "https://zh.wikipedia.org/w/index.php?title=Special:%E6%90%9C%E7%B4%A2&search=%s";
+                }
+                {
+                  name = "Baidu";
+                  keyword = "bd";
+                  url = "https://www.baidu.com/s?wd=%s";
+                }
+                {
+                  name = "Arch manual pages";
+                  keyword = "man";
+                  url = "https://man.archlinux.org/search?q=%s";
+                }
+                {
+                  name = "NixOS Search";
+                  keyword = "nixp";
+                  url = "https://search.nixos.org/packages?channel=unstable&type=packages&query=%s";
+                }
+                {
+                  name = "ArchWiki";
+                  keyword = "aw";
+                  url = "https://wiki.archlinux.org/index.php?title=Special%3ASearch&wprov=acrw1&search=%s";
+                }
+                {
+                  name = "Wikipedia";
+                  keyword = "wk";
+                  url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+                }
+              ];
+            }
+          ];
+        };
         settings = {
           "app.shield.optoutstudies.enabled" = true;
           "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
