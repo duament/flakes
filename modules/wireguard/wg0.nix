2714401bc32a9ff583f4a54d15166e0c75b2569e
Rvfg <i@rvf6.com>
2026-03-25 21:18:46 +0800

    wg0: add dnsmasq config for each peer

--- modules/wireguard/wg0.nix
+++ modules/wireguard/wg0.nix
@@ -97,6 +97,11 @@ let
           type = types.int;
           default = cfg.mtu;
         };
+
+        dnsPort = mkOption {
+          type = types.int;
+          default = 5300 + wg0.peers.${name}.id;
+        };
       };
     };
 in
@@ -289,11 +294,48 @@ in
 
       presets.wireguard.keepAlive.interfaces = map (x: "wg-${x}") clientPeers;
 
-      #presets.wireguard.reResolve.interfaces = optional (cfg.route != null) "wg0";
+      systemd.services = listToAttrs (
+        map (h: {
+          name = "dnsmasq-wg-${h}";
+          value = {
+            description = "Dnsmasq DNS for wg-${h}";
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = self.data.systemdHarden // {
+              Type = "simple";
+              ExecStart = "${pkgs.dnsmasq}/bin/dnsmasq -k -C ${pkgs.writeText "dnsmasq-wg-${h}.conf" ''
+                port=${toString cfg.clientPeers.${h}.dnsPort}
+                listen-address=::
+                listen-address=0.0.0.0
+                interface=lo
+                interface=v1-lan
+                interface=xfrm0
+                interface=wg-router
+                no-resolv
+                server=2606:4700:4700::1111
+                server=2001:4860:4860::8888
+                server=1.1.1.1
+                server=8.8.8.8
+              ''}";
+              Restart = "on-failure";
+              PrivateNetwork = false;
+              RestrictAddressFamilies = [
+                "AF_UNIX"
+                "AF_INET"
+                "AF_INET6"
+                "AF_NETLINK"
+              ];
+            };
+          };
+        }) clientPeers
+      );
 
-      #presets.bpf-mark = mkIf (cfg.route != null) {
-      #  wg0-re-resolve = wgMark;
-      #};
+      presets.bpf-mark = listToAttrs (
+        map (h: {
+          name = "dnsmasq-wg-${h}";
+          value = cfg.clientPeers.${h}.table;
+        }) clientPeers
+      );
     }
 
   ]);
