1463005e5a7710425085f4527d8abace7f2f425b
Rvfg <i@rvf6.com>
2026-04-27 21:33:13 +0800

    modules/common.nix: enforce PQ KexAlgorithms

--- modules/common.nix
+++ modules/common.nix
@@ -132,9 +132,6 @@ with lib;
       KexAlgorithms = [
         "mlkem768x25519-sha256"
         "sntrup761x25519-sha512"
-        "sntrup761x25519-sha512@openssh.com"
-        "curve25519-sha256"
-        "curve25519-sha256@libssh.org"
       ];
       Ciphers = [
         "chacha20-poly1305@openssh.com"
