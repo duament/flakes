2fe53b5d3e3aaf3a872ce11d45f8855f286aa714
Rvfg <i@rvf6.com>
2026-05-17 22:15:27 +0800

    nixos/jp: init

---
+++ nixos/jp/configuration.nix
@@ -0,0 +1,47 @@
+{ ... }:
+{
+  presets.nogui.enable = true;
+
+  #sops.defaultSopsFile = ./secrets.yaml;
+  #sops.secrets = {
+  #  "wireguard_key".owner = "systemd-network";
+  #};
+
+  presets.users.hashedPasswordFile = null;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "jp";
+
+  systemd.network.networks."10-ens3" = {
+    name = "ens3";
+    address = [
+      "2403:71c0:2000:133b::a/64"
+      "172.93.220.78/24"
+    ];
+    dns = [
+      "8.8.8.8"
+    ];
+    networkConfig.IPv6AcceptRA = false;
+    routes = [
+      {
+        Gateway = "2403:71c0:2000::1";
+        GatewayOnLink = true;
+      }
+      {
+        Gateway = "172.93.220.1";
+        GatewayOnLink = true;
+      }
+    ];
+  };
+
+  presets.wireguard.wg0 = {
+    enable = false;
+    mtu = 1400;
+  };
+
+  home-manager.users.rvfg = import ./home.nix;
+
+  presets.nginx.enable = true;
+}
---
+++ nixos/jp/hardware-configuration.nix
@@ -0,0 +1,20 @@
+{ modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "virtio_pci"
+    "virtio_scsi"
+    "ahci"
+    "sd_mod"
+    "sr_mod"
+    "virtio_blk"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/persist".neededForBoot = true;
+}
