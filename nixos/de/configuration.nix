fea2ffe6994cd2dc068d64397ee8fed1ce7a2a45
Rvfg <i@rvf6.com>
2026-06-22 23:50:23 +0800

    nixos/de/swanctl.nix: init

--- nixos/de/configuration.nix
+++ nixos/de/configuration.nix
@@ -1,11 +1,21 @@
 { ... }:
 {
+
+  imports = [
+    # keep-sorted start
+    ./swanctl.nix
+    # keep-sorted end
+  ];
+
   presets.nogui.enable = true;
 
-  #sops.defaultSopsFile = ./secrets.yaml;
-  #sops.secrets = {
-  #  "wireguard_key".owner = "systemd-network";
-  #};
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.secrets = {
+    "pki/ca".mode = "0444";
+    "pki/ybk".mode = "0444";
+    "pki/de-bundle" = { };
+    "pki/de-pkcs8-key" = { };
+  };
 
   boot.loader.grub.enable = true;
 
